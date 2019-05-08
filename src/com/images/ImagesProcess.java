1c7a3ed8365fcde674de14fa821c084d0d44ff3d
hillal <286631@supinfo.com>
2019-05-08 17:43:45 +0200

    Ajout d'un class Icon qui hérite de ImageIcon. Elle comprend deux attributs name et path et udes getters sur ces attributs. Changement de la manière du scale des images, ajout d'une nouvelle mé

--- src/com/images/ImagesProcess.java
+++ src/com/images/ImagesProcess.java
@@ -1,7 +1,5 @@
 package com.images;
 
-import org.omg.CORBA.PRIVATE_MEMBER;
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -16,12 +14,12 @@ public class ImagesProcess {
 
     private String pathToImages;
     private File[] imageFiles;
-    private List<ImageIcon> icones;
+    private List<ImageIcon> icons;
 
     public ImagesProcess(String pathToImages) {
         this.pathToImages = pathToImages;
         this.imageFiles = getImageFilesFromDirectory();
-        this.icones = getImagesFromFiles();
+        this.icons = getImagesFromFiles();
     }
 
     private File[] getImageFilesFromDirectory(){
@@ -30,21 +28,36 @@ public class ImagesProcess {
 
     private List<ImageIcon> getImagesFromFiles(){
         List<ImageIcon> iconsList = new ArrayList<>();
-        BufferedImage image = null;
+        Image image = null;
         for (File file :
                 imageFiles) {
             try {
-                image = ImageIO.read(file);
+                image = scale(ImageIO.read(file), 130, 100);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            ImageIcon icon = new ImageIcon(new ImageIcon(image).getImage().getScaledInstance(130,100, Image.SCALE_DEFAULT));
+            //ImageIcon icon = new ImageIcon(new ImageIcon(image).getImage().getScaledInstance(130,100, Image.SCALE_DEFAULT));
+            Icon icon = new Icon(image, file.getName(), file.getPath());
             iconsList.add(icon);
         }
         return iconsList;
     }
 
-    public List<ImageIcon> getIcones() {
-        return icones;
+    private Image scale(Image source, int width, int height) {
+        /* On crée une nouvelle image aux bonnes dimensions. */
+        BufferedImage buf = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        /* On dessine sur le Graphics de l'image bufferisée. */
+        Graphics2D g = buf.createGraphics();
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g.drawImage(source, 0, 0, width, height, null);
+        g.dispose();
+
+        /* On retourne l'image bufferisée, qui est une image. */
+        return buf;
+    }
+
+    public List<ImageIcon> getIcons() {
+        return icons;
     }
 }

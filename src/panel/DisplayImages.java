15c3b5adff7c9fcbcef2798c1f274e4a4b1f6695
finioknight <47184503+finioknight@users.noreply.github.com>
2019-05-09 10:36:08 +0200

    concatenation hillal tango pour l'affichage des images par nom

--- src/panel/DisplayImages.java
+++ src/panel/DisplayImages.java
@@ -23,18 +23,12 @@ public class DisplayImages extends JPanel {
         for (int i = 0; i<icons.size();i++){
 
             buttonList.add(new JButton(icons.get(i).getName(),icons.get(i)));
-
             buttonList.get(i).setLayout(null);
-
             buttonList.get(i).setPreferredSize(new Dimension(145,130));
-
-            buttonList.get(i).setMaximumSize(new Dimension(130,100));
-
             buttonList.get(i).setBorderPainted(false);
             buttonList.get(i).setHorizontalTextPosition(SwingConstants.CENTER);
             buttonList.get(i).setVerticalTextPosition(SwingConstants.BOTTOM);
             buttonList.get(i).setBackground(Color.WHITE);
-            //buttonList.get(i).setMaximumSize(new Dimension(10,10));
 
             this.add(buttonList.get(i));
         }
